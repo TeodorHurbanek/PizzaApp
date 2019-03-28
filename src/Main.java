1a516e22f546bf6e4d322a6455ba4c3dec28d754
Teodor Hurbánek <teodorhurbanek@gmail.com>
2019-03-28 10:25:49 +0100

    project

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        String pathToMain = "sample/main.fxml";
+
+        URL mainURL = getClass().getResource(pathToMain);
+
+        Parent parent = FXMLLoader.load(mainURL);
+
+        Scene scene = new Scene(parent);
+
+        primaryStage.setFullScreen(true);
+        primaryStage.setTitle("PizzaApp");
+//        primaryStage.getIcons().add(new Image("sk/holic/chat/IT6.png"));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
