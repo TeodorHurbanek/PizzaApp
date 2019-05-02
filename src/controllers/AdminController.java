91ca361db8d5cb5a67d610b813d97ba1a38c2cb3
Teodor Hurbánek <teodorhurbanek@gmail.com>
2019-05-02 10:08:40 +0200

    login a administracia

---
+++ src/controllers/AdminController.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class AdminController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
