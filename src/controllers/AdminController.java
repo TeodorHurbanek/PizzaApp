b6ae14e8cae64afaed3d91f929dd0e8e63efd933
Teodor Hurbánek <teodorhurbanek@gmail.com>
2019-05-09 12:04:50 +0200

    AdminController: ModelTable + DbConnector

--- src/controllers/AdminController.java
+++ src/controllers/AdminController.java
@@ -2,19 +2,25 @@ package controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import javax.swing.text.TabableView;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AdminController implements Initializable {
 
@@ -31,11 +37,54 @@ public class AdminController implements Initializable {
 
     ObservableList<ModelTable> observableList = FXCollections.observableArrayList();
 
+    public void onLogoutButtonClick(ActionEvent event) {
+        System.out.println("Admin have loged out.");
+        try {
+            FXMLLoader fxmlLoader = MainController.getMainController().loadFXML("login");
+
+            Parent parent = fxmlLoader.load();
+
+            MainController.getMainController().mainPage.setCenter(parent);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public FXMLLoader loadFXML(String name) {
+
+        String pathToMain = "../sample /" + name + ".fxml";
+
+        URL mainURL = getClass().getResource(pathToMain);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(mainURL);
+
+        return fxmlLoader;
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        try {
+            Connection connection = DbConnector.getConnection();
+
+            ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM pizza_app.menu");
+
+            while (rs.next()){
+                observableList.add(new ModelTable(
+                        rs.getString("id_pizza"), rs.getString("nazov_pizza"),
+                        rs.getString("ingrediencie"), rs.getString("cena")));
+            }
+
+        } catch (SQLException e) {
+            Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, e);
+        }
+
         col_id.setCellValueFactory(new PropertyValueFactory<>("id"));
         col_nazov.setCellValueFactory(new PropertyValueFactory<>("name"));
         col_ingrediencie.setCellValueFactory(new PropertyValueFactory<>("ingrediencies"));
         col_cena.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        tableAdmin.setItems(observableList);
     }
 }
--- src/controllers/DbConnector.java
+++ src/controllers/DbConnector.java
@@ -1,6 +1,14 @@
 package controllers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnector {
 
+    public static Connection getConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pizza_app", "root", "root");
 
+        return connection;
+    }
 }
